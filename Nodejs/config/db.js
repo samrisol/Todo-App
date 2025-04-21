a723d4cedb348072b217d5b7c2ea42445d55ab55
samrisol <samrawitsola@gmail.com>
2025-04-21 00:34:54 -0700

    initial commit

--- Nodejs/config/db.js
+++ Nodejs/config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-//MONGO_URI_ATLAS=mongodb+srv://todoapp:1212@cluster0.37hyc.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0
+
 const connection = mongoose
   .createConnection(process.env.MONGO_URI_ATLAS)
   .on('open', () => {
--- front_end/lib/Config.dart
+++
@@ -1,7 +0,0 @@
-final String url = 'http://192.168.131.22:3000';
-
-final String registration = "$url/register";
-final String login = "$url/login";
-final String addtodo = "$url/storeTodo";
-final String getToDoList = "$url/getUserTodoList";
-final String deleteTodo = "$url/deleteTodo";
