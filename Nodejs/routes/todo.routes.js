86c8fd2440693bd31f3ebeeb932123830a4edf38
Samrawit <165813012+samrisol@users.noreply.github.com>
2025-04-23 14:09:00 -0700

    initial commit

--- Nodejs/routes/todo.routes.js
+++ Nodejs/routes/todo.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const ToDoController = require('../controller/todo.controller');
 
-router.post("/createToDo", ToDoController.createToDo);
+router.post("/storeTodo", ToDoController.createToDo);
 router.post("/getUserTodoList", ToDoController.getToDoList);
 router.post("/deleteTodo", ToDoController.deleteToDo);
 
